aeec1370330608800b01094a0cd0e623a6673357
vinodh tankasala <vinodh2019@gmail.com>
2018-11-23 16:30:16 -0500

    first commit

---
+++ src/common/Header.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import {link, IndexLink} from 'react-router';
+
+const Header = () => (
+    <div className = "text-center">
+        <nav className = "navbar navbar-default">
+        <IndexLink to = "/" activeClassName = "active">Home</IndexLink>
+        {"|"}
+        <Link to = "Library" activeClassName = "active"> Library</Link>
+        </nav>
+
+    </div>
+);
+
+export default Header;
---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link} from 'react-router';
+
+const HomePage = () => (
+    <div className = "jumotron center">
+        <h1 className = "lead"> wlecome to media lib</h1>
+        <link to ="library">
+        <button className= "btn btn-lg btn-primary">visit library</button>
+        </link>
+    </div>
+)
+export default HomePage;
